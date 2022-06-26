fcce41c11e6e81c1b8607d4d8ae4f0033be8a043
Kevin Sheth <kns98@yahoo.com>
2022-06-26 13:55:33 +0800

    saving

--- CanvasDraw/Form1.Designer.cs
+++ CanvasDraw/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -50,6 +55,32 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -57,9 +88,13 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +104,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- CanvasDraw/Form1.cs
+++ CanvasDraw/Form1.cs
@@ -19,8 +19,11 @@ namespace CanvasDraw
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.Paint += new PaintEventHandler(this.Form1_Paint);
+            saveFileDialog1.Filter = "Image files (*.jpg, *.png, *.bmp) | *.jpg; *.png; *.bmp";
 
         }
+
+        private Stack <(Pen,Rectangle)> objects = new Stack<(Pen,Rectangle)>();
 
         private Rectangle RcDraw = new Rectangle();
         private float PenWidth = 5;
@@ -58,6 +61,11 @@ namespace CanvasDraw
 
         private void Form1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
         {
+            //Keep a stash of objects for saving
+            Pen pen = new Pen(Color.Blue, PenWidth);
+            Rectangle rect = new Rectangle(new Point(RcDraw.X,RcDraw.Y), new Size(RcDraw.Width,RcDraw.Height));
+            objects.Push((pen, rect));
+
             // Draw the rectangle...
             e.Graphics.DrawRectangle(new Pen(Color.Blue, PenWidth), RcDraw);
         }
@@ -68,26 +76,46 @@ namespace CanvasDraw
 
         }
 
-        private void SaveControlImage(Control ctr)
+        private void SaveControlImage()
         {
+            this.label1.SendToBack();
+            this.label2.SendToBack();
+            this.menuStrip1.Visible = false;
+            this.label1.Visible = false;
+            this.label2.Visible = false;
+
+            Control ctr = this;
             try
             {
-                var imagePath = @"C:\Image.png";
-                Image bmp = new Bitmap(ctr.Width, ctr.Height);
-                var gg = Graphics.FromImage(bmp);
-                var rect = ctr.RectangleToScreen(ctr.ClientRectangle);
-                gg.CopyFromScreen(rect.Location, Point.Empty, ctr.Size);
-                bmp.Save(imagePath);
+                saveFileDialog1.ShowDialog();
+                var imageOutputStream = saveFileDialog1.OpenFile();
+                using (Image bmp = new Bitmap(ctr.Width, ctr.Height))
+                {
+                    
+                    var gg = Graphics.FromImage(bmp);
+
+                    foreach (var _obj in this.objects)
+                    {
+                        gg.DrawRectangle(_obj.Item1,_obj.Item2);
+                    }
+                    
+                    bmp.Save(imageOutputStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                //
+                MessageBox.Show(e.Message);
             }
+
+            this.menuStrip1.Visible = true;
+            this.label1.Visible = true;
+            this.label2.Visible = true;
+            
         }
 
-        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        private void saveToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
-            //SaveControlImage();
+            SaveControlImage();
         }
     }
 }
